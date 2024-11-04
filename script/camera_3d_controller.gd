d692493d8e76dfc91e8191beb9fa0a9e73ed6022
lazy <98337661+lazy-top@users.noreply.github.com>
2024-11-04 11:44:07 +0800

    更新

--- script/camera_3d_controller.gd
+++ script/camera_3d_controller.gd
@@ -1,8 +1,18 @@
 extends Camera3D
-
-
+#鼠标左键按下的状态，false 为并未按下
+@onready var mouse_left_press_state=false
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("mouse_wheel_up"):
 		print('-')
+	if Input.is_action_just_pressed("mouse_wheel_down"):
+		print('-')
+	if Input.is_action_just_pressed("mouse_left_press"):
+		mouse_left_press_state=true
+	if Input.is_action_just_released("mouse_left_press"):
+		mouse_left_press_state=false
+#    是否按下左键
+	if mouse_left_press_state:
+		var mouse_postion=get_viewport().get_mouse_position()
+#		
 	pass
